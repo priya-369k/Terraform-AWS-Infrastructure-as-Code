7fd3d7cadbf149e9fa86c131ee3e269e74ea9101
priya-369k <npr.priya@outlook.com>
2025-10-26 18:27:43 +0000

    Add files via upload

---
+++ Keypair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "dove-key" {
+  key_name   = "dove-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII8TyMX3MS5sZJEHB3XfvkDqnsUcjJXEFnIlSZJqTVqu Nikki@LAPTOP-0H3E16BO"
+}
